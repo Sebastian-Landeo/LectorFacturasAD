57409323c232046eeb5487f664239480ee8e6cad
Sebastian Landeo <sebastian.landeo@unmsm.edu.pe>
2025-03-23 12:25:18 -0500

    Extracción de textos de pdf

--- funciones.py
+++ funciones.py
@@ -2,14 +2,24 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+# pip install pymupdf
+import fitz #PyMuPDF
 # Cargar variables de entorno desde .env
 load_dotenv()
 # Obtener la clave de la API desde el archivo .env
 api_key = os.getenv("GEMINI_API_KEY")
-print(f"API Key: {api_key}")
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel(model_name="gemini-2.0-flash")
 
-consulta = "Quien descubrio america"
-response = model.generate_content(consulta)
-print(response.text)
+
+# print(f"API Key: {api_key}")
+# genai.configure(api_key=api_key)
+# model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+# consulta = "Quien descubrio america"
+# response = model.generate_content(consulta)
+# print(response.text)
+
+def extraer_texto_pdf(ruta_pdf):
+    doc = fitz.open(ruta_pdf) #Abrir el archivo PDF
+    texto = "\n".join([pagina.get_text() for pagina in doc]) #Extraer el texto de cada página
+    return texto
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from sqlalchemy import create_engine
+import funciones
 
 # Crear un dataframe vacío para almacener todos las facturas
 df = pd.DataFrame()
@@ -12,7 +13,11 @@ for carpeta in sorted(os.listdir("./Facturas")):
     # Recorrer todos los archivos dentro de la carpeta
     for archivo in os.listdir(ruta_carpeta):
         ruta_pdf = os.path.join(ruta_carpeta, archivo)
-        
         print(f"Procesando {ruta_pdf}")
+
+        # Extraer el texto de la factura
+        texto_no_estructurado = funciones.extraer_texto_pdf(ruta_pdf)
+        print("-" * 50)
+        print(texto_no_estructurado)
 
 print("finalizado")
